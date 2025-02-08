79330b4cb24a832543b4cdaa75dea200815eda8c
Svendolin <sven0815@gmx.ch>
2025-02-08 23:58:53 +0100

    Bugfix for vercel at page.tsx V5

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,7 @@
+// Over here we are fetching the product details from the database and displaying it on the page.
+// We are using the product id from the url to fetch the product details from the database.
+// => This is the place where we display the product details like name, price, description, image, etc.
+
 import { db } from '@/db'
 import { productsTable } from '@/db/schema'
 import { notFound } from 'next/navigation'
@@ -7,32 +11,49 @@ import Image from 'next/image'
 import { Check, Shield } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 
-interface PageProps {
-  params: { productId: string };
-}
 
-const Page = async ({ params }: PageProps) => {
-  if (!params.productId) return notFound();
+const Page = async ({ params }: { params: { productId: string } }) => {
+  const { productId } = params
+
+  // If the product id is not present in the url, we are returning a 404 page.
+  if (!productId) return notFound()
 
   const [product] = await db
     .select()
     .from(productsTable)
-    .where(eq(productsTable.id, params.productId));
+    .where(eq(productsTable.id, productId))
 
-  if (!product) return notFound();
+  if (!product) return notFound()
+  // BackButton is a custom component that we have created to navigate back to the previous page.
+  /* Up next the followning lines will be:
 
+  - BackButton Component to get back to the 3  models overview
+  - Product name (Title)
+  - Product image
+  - Product price (toFixed(2) means that the price will be displayed with 2 decimal places)
+  - Product description
+  - A message that says that the product is eligible for express delivery
+  - A button to add the product to the cart
+  - A message that says that the product is eligible for a 30-day return guarantee
+
+  (Many of the icons are from Lucide, a library of icons that we are using in this project)
+  
+  
+  */
   return (
     <div className='py-8 pb-8 px-12 divide-y divide-zinc-100 bg-white shadow-md rounded-b-md'>
       <div>
-        <BackButton />
+        
+        
+        <BackButton /> 
 
-        <div className='mt-4'>
+        <div className='mt-4'> 
           <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
             {product.name}
           </h1>
         </div>
 
-        <div className='aspect-square my-6 border border-border w-58 h-58'>
+        <div className='aspect-square my-6 border border-border w-58 h-58'> 
           <div className='relative bg-zinc-100 w-full h-full overflow-hidden rounded-xl'>
             <Image
               fill
@@ -50,7 +71,7 @@ const Page = async ({ params }: PageProps) => {
               Approximate value: ${product.price.toFixed(2)}
             </p>
             <p className='ml-2 text-sm text-muted-foreground'>
-              (Price corresponds to original packaging)
+            (Price corresponds to original packaging)
             </p>
           </div>
 
@@ -59,8 +80,9 @@ const Page = async ({ params }: PageProps) => {
               Release date: {product.age}
             </p>
             <p className='ml-2 text-sm text-muted-foreground'>
-              (of the model as shown above)
+            (of the model as shown above)
             </p>
+            
           </div>
 
           <div className='mt-4 space-y-6'>
@@ -79,21 +101,21 @@ const Page = async ({ params }: PageProps) => {
       </div>
 
       <div className='mt-6'>
-        <a href="mailto:support@toycarsaddict.club">
-          <Button className='w-full mt-10'>CONTACT US</Button>
-        </a>
+      <a href="mailto:support@toycarsaddict.club">
+        <Button className='w-full mt-10'>CONTACT US</Button>
+      </a>
 
         <div className='mt-6 text-center'>
           <div className='inline-flex text-sm text-medium'>
             <Shield className='mr-2 h-5 w-5 flex-shrink-0 text-gray-400' />
             <span className='text-muted-foreground hover:text-gray-700'>
-              Part of the collection of Toycarsaddict_Daily
+            Part of the collection of Toycarsaddict_Daily
             </span>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
